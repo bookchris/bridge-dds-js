fdd8f444c35c07a428598a64c63233661bf8a7ea
Chris Book <chrisbook@gmail.com>
2025-03-26 22:43:17 -0400

    version 104
    - fix undefined setValue call
    - fix broken dds.js exports
    - add an expensive example to compute
    - enable memory growth that is required for expensive example

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { Dds } from "./api";
 import { Direction, Trump } from "./constants";
 import { default as DdsLoader } from "./dds";
 
-export { Dds, DdsLoader as default, Direction, Trump };
+export { DdsLoader as default, Direction, Trump };
